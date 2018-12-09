6b7c2b2a40c0177eda014fe88031501c5f546285
Eduard Shustrov <eshustrov@gmail.com>
2018-12-09 22:00:08 +0100

    small cleanup

--- src/main/java/info/devtraining/accountinformationbackservice/AccountInformationBackServiceApplication.java
+++ src/main/java/info/devtraining/accountinformationbackservice/AccountInformationBackServiceApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AccountInformationBackServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AccountInformationBackServiceApplication.class, args);
-	}
+    public static void main(final String... args) {
+        SpringApplication.run(AccountInformationBackServiceApplication.class, args);
+    }
 }
